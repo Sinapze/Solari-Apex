40fed0c51390ceb7ba613fb15b4926657351977f
Sinapze <sinapselp@gmail.com>
2015-09-24 20:13:48 -0500

    Finished up daybreak. Auto-sucks torches, keeps count, etc. :)

--- src/main/com/sinapze/solariapex/item/equipment/armor/blyze/ItemBlyzeArmor.java
+++ src/main/com/sinapze/solariapex/item/equipment/armor/blyze/ItemBlyzeArmor.java
@@ -5,13 +5,18 @@ import com.sinapze.solariapex.item.ItemBlyzeChunk;
 import com.sinapze.solariapex.item.ItemEvoliteIngot;
 import com.sinapze.solariapex.item.ModItems;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemArmor.ArmorMaterial;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ISpecialArmor;
 
 public class ItemBlyzeArmor extends ItemArmor implements ISpecialArmor{
@@ -65,5 +70,36 @@ public class ItemBlyzeArmor extends ItemArmor implements ISpecialArmor{
 	public void damageArmor(EntityLivingBase entity, ItemStack stack, DamageSource source, int damage, int slot) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	//private Block[] blocksToChange = new Block[10]; //place to store last 10 blocks
+	//private int toLight = 9;
+	//private int toDelight = 0;
+	
+	@Override
+	public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack)
+	{
+		/* This function was supposed to light up the last 10 blocks.
+		 * However world.getBlock returns a block type, and therefore it applies light to all blocks
+		 * of that type. aka giving every single grass block a light level of 15
+		if (player.getCurrentArmor(0) != null && player.getCurrentArmor(0).getItem().equals(ModItems.blyzeBoots) && player.getCurrentArmor(1) != null && player.getCurrentArmor(1).getItem().equals(ModItems.blyzeLegs) && player.getCurrentArmor(2) != null && player.getCurrentArmor(2).getItem().equals(ModItems.blyzeChest) && player.getCurrentArmor(3) != null && player.getCurrentArmor(3).getItem().equals(ModItems.blyzeHelm))
+		{
+			int x = (int) player.lastTickPosX;
+			int y = (int) player.lastTickPosY -1;
+			int z = (int) player.lastTickPosZ;
+			if (world.getBlock(x, y, z) != blocksToChange[toLight] && !world.getBlock(x, y, z).isAir(world, x, y, z) && world.getBlock(x, y, z).isOpaqueCube())
+			{
+				toLight = (toLight + 1) % blocksToChange.length; //loops back to 0 when it would reach 10, otherwise increments
+				toDelight = (toDelight + 1) % blocksToChange.length; //loops back to 0 when it would reach 10, otherwise increments
+				blocksToChange[toLight] = world.getBlock(x, y, z);
+				
+				System.out.println("We want to light" + blocksToChange[toLight]); //testing
+				System.out.println("We want to delight" + blocksToChange[toDelight]);
+				//blocksToChange[toLight].setLightLevel(1.0F);
+				//blocksToChange[toDelight].setLightLevel(0.0F);
+			}
+			
+		}
+		*/
 	}
 }
--- src/main/com/sinapze/solariapex/item/equipment/tool/ItemDaybreak.java
+++ src/main/com/sinapze/solariapex/item/equipment/tool/ItemDaybreak.java
@@ -2,6 +2,7 @@ package com.sinapze.solariapex.item.equipment.tool;
 
 import java.util.List;
 
+import com.sinapze.solariapex.SolariApex;
 import com.sinapze.solariapex.block.ModBlocks;
 import com.sinapze.solariapex.item.ItemBase;
 import com.sinapze.solariapex.item.ModItems;
@@ -10,54 +11,70 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockTorch;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
 @SuppressWarnings("unused")
 
 public class ItemDaybreak extends ItemBase{
 
+	IIcon onIcon; //create icons names to hold our on and off icons
+	IIcon offIcon;
+	
 	public ItemDaybreak() 
 	{
 		super("daybreak");
-		this.setMaxDamage(100);
-		this.setMaxStackSize(1);
+		this.setMaxStackSize(1); //makes it so you cant have more than 1 in a slot
+		setHasSubtypes(true); //tells the game we have subtypes (for the different textures)
 	}
 
 	
 	@Override
 	public void onCreated(ItemStack itemStack, World world, EntityPlayer player) 
 	{
-		itemStack.setItemDamage(1);
+		//when the item is created, we attach a nbt tag compound to it and then set a long with key "torchNum"
+		//initially we want this to be 0, and it will increase as torches are added to it
+		itemStack.stackTagCompound = new NBTTagCompound();
+		itemStack.stackTagCompound.setLong("torchNum", 0);
 	}
 	
-	//@SuppressWarnings("rawtypes")
-	@SuppressWarnings("unchecked")
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	@SideOnly(Side.CLIENT)
     public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean adv) 
 	{
-		list.add(EnumChatFormatting.GOLD + "Torches Stored: " + (itemStack.getMaxDamage() - itemStack.getItemDamage()));
+		//add a check to see if there is any nbttag yet, because when in a creative menu there isnt and it crashes when it tries to render it
+		if (itemStack.stackTagCompound != null)
+		{
+			long torchNum = itemStack.stackTagCompound.getLong("torchNum");
+			list.add(EnumChatFormatting.YELLOW + "Torches Stored: " + torchNum);
+		}
+		else //if there isnt a nbt tag just put a placeholder until it is actually spawned in and onUpdate gives it one
+			list.add(EnumChatFormatting.YELLOW + "Torches Stored: ");
 	}
 	
 	@Override
 	public boolean onItemUse(ItemStack itemStack, EntityPlayer player, World world, int x, int y, int z, int side, float par8, float par9, float par10)
 	{
 		boolean isSolid = false; //create a flag to use later and initialize it to false
-		
+		long torchNum = itemStack.stackTagCompound.getLong("torchNum"); //the NBT tag that holds the amount of torches stored
 		//we first check to see if we have any stored torches in the daybreak. otherwise we cant place one
-		if (itemStack.getMaxDamage() - itemStack.getItemDamage() > 0)
+		if (torchNum > 0)
 		{
 			//then first check if the block we are clicking on is some sort of "replacable" like grass, snow, lava, water etc
 			if(world.getBlock(x, y, z).isReplaceable(world, x, y, z))
 			{
 				world.setBlock(x, y, z, Blocks.torch); //if it is replacable we replace it with the torch
-				itemStack.damageItem(1, player);
+				itemStack.stackTagCompound.setLong("torchNum", torchNum - 1); //if we placed the torch, we want to update the torchNum by subtracting 1
 				return true;
 			}
 			
@@ -91,6 +108,7 @@ public class ItemDaybreak extends ItemBase{
 	//function which handles trying and placing the actual torches
 	public boolean tryPlaceTorch(ItemStack itemStack, EntityPlayer player, World world, int x, int y, int z, boolean tryAdvanced)
 	{
+		long torchNum = itemStack.stackTagCompound.getLong("torchNum"); //the NBT tag that holds the amount of torches stored
 		//first we check to see if the block we are trying to place a torch in is air or replacable
 		//if it isnt we dont want to be able to place a torch there
 		if(world.isAirBlock(x, y, z) || world.getBlock(x, y, z).isReplaceable(world, x, y, z))
@@ -103,7 +121,7 @@ public class ItemDaybreak extends ItemBase{
 				if(world.getBlock(x,y-1,z).isOpaqueCube() || world.getBlock(x-1,y,z).isOpaqueCube() || world.getBlock(x+1,y,z).isOpaqueCube() || world.getBlock(x,y,z-1).isOpaqueCube() || world.getBlock(x,y,z+1).isOpaqueCube())
 				{
 					world.setBlock(x, y, z, Blocks.torch);
-					itemStack.damageItem(1, player);
+					itemStack.stackTagCompound.setLong("torchNum", torchNum - 1);
 					return true;
 				}
 			}
@@ -113,7 +131,7 @@ public class ItemDaybreak extends ItemBase{
 			else
 			{
 				world.setBlock(x, y, z, Blocks.torch);
-				itemStack.damageItem(1, player);
+				itemStack.stackTagCompound.setLong("torchNum", torchNum - 1);
 				return true;
 			}
 		}
@@ -121,22 +139,53 @@ public class ItemDaybreak extends ItemBase{
 	}
 	
 	
-	//TODO Add torch sucking? Light under feet?
 	@Override
 	public void onUpdate(ItemStack itemStack, World world, Entity entity, int p_77663_4_, boolean p_77663_5_)
 	{
+		EntityPlayer player = (EntityPlayer) entity; //cast our entity as a player so we can use player functions
+		//a check to make sure the nbt tag for storing the number of torches exists. if it doesnt (pulled from creative, glitch) we make it and set torchNum to 0
+		if (itemStack.stackTagCompound == null)
+		{
+			itemStack.stackTagCompound = new NBTTagCompound();
+			itemStack.stackTagCompound.setLong("torchNum", 0);
+		}
+		//make an integer array that is the length of the players inventory
+		int[] slots = new int[player.inventory.getSizeInventory() - player.inventory.armorInventory.length];
 		
-	}
-	//TODO testing purposes only. Remove before actual release
-	@Override
-	public boolean showDurabilityBar(ItemStack itemStack)
-	{
-		return true;
+		for (int i = 0; i < slots.length; i++) //start a for loop to go through each slot of inventory
+		{
+			long torchNum = itemStack.stackTagCompound.getLong("torchNum"); //get our current amount stored before each iteration
+			ItemStack stack = player.inventory.getStackInSlot(i); //get the stack we are currently looking at
+			if(stack == null) //if the slot is empty we just want to go to the next iteration
+				continue;
+			else if (stack.getItem() == Item.getItemFromBlock(Blocks.torch)) //if the slot contains torches
+			{
+				itemStack.stackTagCompound.setLong("torchNum", torchNum + stack.stackSize); //we add the stacksize to our tag
+				player.inventory.setInventorySlotContents(i, null); //and delete the torches out of the inventory
+			}
+		}
+		//every tick, we check to see if the number of torches is greater than 0. if it is, we set damage to 1 (on) or if it isnt set it to 0 (off)
+		if (itemStack.stackTagCompound.getLong("torchNum") > 0)
+			itemStack.setItemDamage(1);
+		else
+			itemStack.setItemDamage(0);
 	}
 	
-	//allows the tool to be reparable. tests if the item trying to be used is equal to a torch
-	public boolean getIsRepairable(ItemStack toRepair, ItemStack stack)
+	//this is where we define where the textures come from for the on and off states
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IIconRegister reg) 
 	{
-		return stack.getItem() == Item.getItemFromBlock(Blocks.torch);
+		offIcon = reg.registerIcon(SolariApex.MODID + ":/tools/daybreak_0");
+		onIcon = reg.registerIcon(SolariApex.MODID + ":/tools/daybreak_1");
 	}
+	
+	//this function is used to change the texture based on the item damage. 0 == off, 1 == on
+	@Override
+	@SideOnly(Side.CLIENT)
+	public IIcon getIconFromDamage(int damage) 
+	{
+		return (damage == 1) ? onIcon : offIcon;
+	}
+
 }
